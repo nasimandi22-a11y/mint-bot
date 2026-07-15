4912bc1a5c1bb65009b2463b7b268c23e2c2d635
nasimandi22-a11y <nasimandi22@gmail.com>
2026-07-15 20:05:27 +0700

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ TOKEN = os.getenv("BOT_TOKEN")
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     keyboard = [[InlineKeyboardButton("🛒 Cek Stok", callback_data='cek_stok')]]
     reply_markup = InlineKeyboardMarkup(keyboard)
-    await update.message.reply_text("Selamat datang di Mint Mobile Store!", reply_markup=reply_markup)
+    await update.message.reply_text("Selamat datang di Mint Mobile Store Distributor!", reply_markup=reply_markup)
 
 async def cek_stok(update: Update, context: ContextTypes.DEFAULT_TYPE):
     query = update.callback_query
